66e5a4f202813046ce3751a67bf66de32a09fc00
Rodrigo <rodrigoramalho.dev@gmail.com>
2019-09-22 02:15:28 -0300

    Deletando relaçoes entre bloqueados

--- server/routes/block.js
+++ server/routes/block.js
@@ -10,15 +10,27 @@ router.get('/b/:username', async (req, res) => {
     const blocking = req.session.userId;
     if(!blocking) res.send("Not Logged");
     let query = "SELECT id FROM usuario WHERE username = $1;"
-    db.query(query, [username]).then((row) => { 
+    db.query(query, [username]).then((row) => {
+    
         if(!Array.isArray(row.rows) || !row.rows.length) {
             throw new Error("User not found"); return
         };
-        blockedId = row.rows[0].id;
+        let blockedId = row.rows[0].id;
         // console.log(`${req.session.user}(${blocking}) is blocking ${username}(${blockedId})!`);
         db.query("INSERT INTO bloquear (iduser, idbloqueado) VALUES ($1, $2)", [blocking, blockedId]).then((row) => {
+            deletarSeguindo =`DELETE FROM seguindo WHERE seguindo.idUser = '${blocking}' AND seguindo.idSeguido = '${blockedId}' OR  seguindo.idUser = '${blockedId}' AND seguindo.idSeguido = '${blocking}'`;
+            db.query(deletarSeguindo, []).catch((err)=>{
+                console.log(err);
+            });
+
+            deletarComentario = `DELETE FROM comentario WHERE comentario.idComentador = '${blocking}' AND comentario.idPost IN (SELECT post.idPost FROM post WHERE post.idUser = '${blockedId}') OR comentario.idComentador = '${blockedId}' AND comentario.idPost IN (SELECT post.idPost FROM post WHERE post.idUser = '${blocking}')`; 
+            db.query(deletarComentario, []).catch((err)=>{
+                console.log(err);
+            });
+            
             res.send({blocking, blocked: blockedId});
         }).catch((err) => {
+            console.log(err);
             res.status(404).send(err);
         });;
     }).catch((err) => {
